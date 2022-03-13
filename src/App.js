bf24e287524cf03db4eaa8f27e0e30a182597884
Matias Menendez <matias.95@hotmail.es>
2022-03-13 18:03:23 -0300

    correcion de colocacion item list container

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Navbar />
-        <ItemListContainer />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <ItemListContainer />
     </div>
   );
 }

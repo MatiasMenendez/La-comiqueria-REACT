07e0bbac46aef5aa30c461b567ed5d76249117ff
Matias Menendez <matias.95@hotmail.es>
2022-04-19 23:54:45 -0300

    last details

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -32,8 +32,8 @@ const ItemListContainer = () => {
     <>
     {
       loading
-      ? <div class="spinner-border text-primary" role="status">
-      <span class="visually-hidden">Loading...</span>
+      ? <div className="spinner-border text-primary" role="status">
+      <span className="visually-hidden">Loading...</span>
     </div>
       : <ComicList listaComics={listaComics}/>
     }
